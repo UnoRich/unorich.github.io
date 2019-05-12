d9ce3d4d236c83fa743a6fa408a54c182ab9da20
UnoRich <wonyeong91@gmail.com>
2019-05-12 16:41:40 +0900

    Updates

---
+++ precache-manifest.73833a7cd3af1e67592088fed1a801e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8118a6ed143d972c0286519dc5a01240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b42f8befeb99921cbe48",
+    "url": "/static/css/main.b129a085.chunk.css"
+  },
+  {
+    "revision": "429a06b46b755220a782",
+    "url": "/static/js/2.38ad3831.chunk.js"
+  },
+  {
+    "revision": "b42f8befeb99921cbe48",
+    "url": "/static/js/main.c1ad4cad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
