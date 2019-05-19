e1858f315cc4ef25b61965df515a7cfae0b6d61a
UnoRich <wonyeong91@gmail.com>
2019-05-19 15:48:07 +0900

    bug: change browser router to hash router - cause gh page does not support client routing
    Signed-off-by: UnoRich <wonyeong91@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Main from 'pages/Main';
 import store from 'store';
@@ -9,9 +9,9 @@ class App extends Component {
   render() {
     return (
       <Provider store={store()}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={'/'}>
           <Main />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     );
   }
